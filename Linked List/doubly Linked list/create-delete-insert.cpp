1c13560e89fe51df06b5a88d21951bcfe07f8dd7
Gaurav Machade <143127193+gaurav-04-takes@users.noreply.github.com>
2024-04-11 19:31:51 +0530

    doubly linked list

--- Linked List/doubly Linked list/create-delete-insert.cpp
+++ Linked List/doubly Linked list/create-delete-insert.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Node
 {
 public:
+    Node *prev;
     int data;
     Node *next;
 } *head = NULL;
@@ -13,7 +14,7 @@ class LinkedList
     Node *last, *t;
 
 public:
-    void create(int A[0], int n);
+    void create(int A[], int n);
     void display();
     void insert(int index, int n);
     void Delete(int index);
@@ -23,8 +24,8 @@ public:
 int LinkedList::length()
 {
     Node *p = head;
-    int length = 1;
-    while (p->next != head)
+    int length = 0;
+    while (p)
     {
         length++;
         p = p->next;
@@ -35,15 +36,16 @@ int LinkedList::length()
 void LinkedList::create(int A[], int n)
 {
     head = new Node;
+    head->prev = NULL;
+    head->next = NULL;
     head->data = A[0];
-    head->next = head;
     last = head;
-
     for (int i = 1; i < n; i++)
     {
         t = new Node;
         t->data = A[i];
-        t->next = head;
+        t->prev = last;
+        t->next = last->next;
         last->next = t;
         last = t;
     }
@@ -52,9 +54,7 @@ void LinkedList::create(int A[], int n)
 void LinkedList::display()
 {
     Node *p = head;
-    cout << p->data << " ";
-    p = p->next;
-    while (p != head)
+    while (p)
     {
         cout << p->data << " ";
         p = p->next;
@@ -64,11 +64,13 @@ void LinkedList::display()
 void LinkedList::insert(int index, int n)
 {
     Node *p = head;
+
     if (!p)
     {
         head = new Node;
+        head->prev = NULL;
+        head->next = NULL;
         head->data = n;
-        head->next = head;
         last = head;
     }
     else
@@ -77,66 +79,29 @@ void LinkedList::insert(int index, int n)
         t->data = n;
         if (index == 1)
         {
-
-            while (p->next != head)
-            {
-                p = p->next;
-            }
+            t->prev = NULL;
             t->next = head;
-            p->next = t;
+            head->prev = t;
             head = t;
         }
         else
         {
-            int count = 0;
-            Node *q = head;
-            while (p->next != head)
+            if (index < 0 || index > length())
+                return;
+            else
             {
-                if (count == index - 1)
+                for (int i = 1; i < index - 1; i++)
                 {
-                    t->next = q->next;
-                    q->next = t;
-                    return;
-                }
-                else
-                {
-                    count++;
-                    q = p;
                     p = p->next;
                 }
+                t->next = p->next;
+                t->prev = p;
+                p->next->prev = t;
+                p->next = t;
+                // t->prev = p->prev;
+                // p->prev->next = t;
+                // t->next = p;
             }
-        }
-    }
-}
-
-void LinkedList::Delete(int index)
-{
-    Node *p = head;
-    Node *q = head;
-    if (index == 1)
-    {
-        while (q->next != head)
-        {
-            q = p;
-            p = p->next;
-        }
-        q->next = head->next;
-        head = head->next;
-        delete p;
-    }
-    else
-    {
-        if (index > length())
-            return;
-        else
-        {
-            for (int i = 0; i < index - 1 && p->next != head; i++)
-            {
-                q = p;
-                p = p->next;
-            }
-            q->next = p->next;
-            delete p;
         }
     }
 }
@@ -149,13 +114,9 @@ int main()
     l.create(A, n);
     l.display();
     cout << endl;
-    l.insert(1, 1);
+    l.insert(3, 1);
     l.display();
     cout << endl;
-    l.Delete(13);
-    l.display();
-    cout << endl;
-    l.insert(6, 1);
-    l.display();
+
     return 0;
 }
